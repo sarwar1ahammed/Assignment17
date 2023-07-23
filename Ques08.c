e11b6d099bcc270f54750f1119fa0ebf125738e5
Sarwar Ahammed <110556391+sarwar1ahammed@users.noreply.github.com>
2023-07-23 10:37:03 +0530

    Ques08.c

--- Ques08.c
+++ Ques08.c
@@ -1,21 +1,39 @@
+// #include <stdio.h>
+// // Copy one string to another string.
+// int main()
+// {
+//     char str1[20], str2[20], temp[50];
+//     printf("Enter first string : ");
+//     fgets(str1, 20, stdin);
+//     printf("Enter second string : ");
+//     fgets(str2, 20, stdin);
+//     int i, j;
+//     for (i=0; str1[i]; i++)
+//         temp[i] = str1[i];
+//     for (j=0; str2[j]; j++)
+//         temp[i+j] = str2[j];
+ 
+//     printf("%s", temp);
+
+
+//     printf("\n");
+//     return 0;
+// }
 #include <stdio.h>
-// Copy one string to another string.
+#include <string.h>
+
 int main()
 {
-    char str1[20], str2[20], temp[50];
-    printf("Enter first string : ");
+    char str1[20], str2[20];
+    printf("Enter a string : ");
     fgets(str1, 20, stdin);
-    printf("Enter second string : ");
-    fgets(str2, 20, stdin);
-    int i, j;
-    for (i=0; str1[i]; i++)
-        temp[i] = str1[i];
-    for (j=0; str2[j]; j++)
-        temp[i+j] = str2[j];
+
+    printf("Given string in String1 : %s", str1);
  
-    printf("%s", temp);
-
-
+    strcpy(str2, str1);
+    
+    printf("String in String2 is : %s", str2);
+  
     printf("\n");
     return 0;
 }
